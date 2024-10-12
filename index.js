00983bdb12d5a811179885a990880804c561b861
yosoycaldegit config --global user.name yosoycalde <juancalderon2005.18@gmail.com>
2024-10-11 20:23:21 -0500

    h

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-hola mundo
+// tipos de variables 
+// var numero =4; = no se utiliza por actualizar 
+let saludo = "saludo";
+const pi = 3.1416;
+
+// tipos de datos
+//numeros
+ let entero = 10;
+ let decimal = 3.1416;
+//string = texto
+let pregunta = "¿Que hora es?"
+let respuesta = 'son '
