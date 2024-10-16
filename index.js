7336ab5ded6c3a8f6307d96be155a967a0682707
yosoycalde <juancalderon2005.18@gmail.com>
2024-10-16 18:37:47 -0500

    h

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const pi = 3.1416;
 
 // tipos de datos
 //numeros
+
  let entero = 10;
  let decimal = 3.1416;
+
 //string = texto
 let pregunta = "¿Que hora es?"
 let respuesta = 'son las 4 pm'
@@ -21,3 +23,7 @@ console.long(valor); //undefine
 
 //null
 let nota = null;
+hola
+
+
+
