114819e8c411ad5a6f6d25f60e1ad809d1e6c29a
yosoycalde <juancalderon2005.18@gmail.com>
2024-10-16 20:29:43 -0500

    terminado el 16-10-2024

--- condicionales.js
+++ condicionales.js
@@ -7,3 +7,10 @@ let edad = 18;
 if (edad >= 18) {
   console.log("Eres mayor de edad");
 }
+
+//condicional if-else
+if (edad >= 18) {
+  console, long("Eres mayor de edad");
+} else {
+  console, long("Eres menor de edad");
+}
